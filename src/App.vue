ef03a40212b165adcd4ddac702a7e6fc5737e606
ruirui <yruirui96@gmail.com>
2024-05-20 18:15:21 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <tina />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import tina from './components/tina.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    tina
+  }
+}
+</script>
---
+++ src/components/tina.vue
@@ -0,0 +1,13 @@
+<template>
+   我的一个组件
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
