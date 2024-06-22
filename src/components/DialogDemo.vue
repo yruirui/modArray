da3fb1033ade02212ada687adca5a96a5ffb5c5e
ruirui <yruirui96@gmail.com>
2024-06-22 17:25:06 +0800

    dialog的html

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,15 @@
 <template>
   dialog组件
+  <h1>示例1</h1>
+   <Dialog></Dialog>
 </template>
 
-<script>
-export default {
+<script lang="ts">
 
+import Dialog from '../lib/Dialog .vue'
+
+export default {
+components:{Dialog }
 }
 </script>
 
---
+++ src/lib/Dialog .vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div  class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+        <p>one</p>
+        <p>two</p>
+    </main>
+    <footer>
+        <Button>ok</Button>
+        <Button>cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Button from '../lib/Button.vue'
+export default {
+components:{Button}
+}
+</script>
+
+<style>
+
+</style>
