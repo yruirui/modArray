0faf451e6bc000c74df96f1e56c39b193f50b4d5
ruirui <yruirui96@gmail.com>
2024-06-22 12:25:33 +0800

    looading完成

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -30,7 +30,10 @@
     <Button theme="text" disabled>禁用按钮</Button>
    </div>
    <h1>示例五</h1>
-   <div><Button loading></Button></div>
+   <div>
+    <Button loading>加载中</Button>
+    <Button >加载完毕</Button>
+   </div>
 </template>
 
 <script lang="ts" >
