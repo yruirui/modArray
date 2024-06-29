36837739112c0d7802d949d8b904e6e474b3422c
ruirui <yruirui96@gmail.com>
2024-06-29 19:59:00 +0800

    选中导航

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Tab示例1</h1>
-  <Tabs>
+  因为使用的key值为title，所以Tab组件的title不能重复，不然会出现问题
+  <Tabs selected="导航1">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,20 @@
 <template>
- <div>这里是tabs组件</div> 
- <div v-for="(t ,index) in Titles" :key="index" >{{ t }}</div>
- <component v-for='(c ,index) in defaults' :is="c"  :key="index"/>
+ <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+        <div class="gulu-tabs-nav-item" :class="{selected:t==selected}" v-for="(t ,index) in Titles" :key="index" >{{ t }}</div>
+    </div>
+    <div class="gulu-tabs-content" >
+        <component v-for='(c ,index) in defaults' :is="c"  :key="index"/> 
+    </div>
+ </div>
 </template>
 
 <script lang="ts">
 import Tab from '../lib/Tab.vue'
 export default{
+    props:{
+        selected:{type:String}
+    },
     setup(props,context){
        const defaults=context.slots.default!()
        defaults.forEach((tag)=>{
@@ -25,6 +33,26 @@ export default{
 }
 </script>
 
-<style>
+<style lang="scss">
+$blue:#40a9ff;
+$color:#333;
+$border-color:#d9d9d9;
+
+.gulu-tabs{
+    &-nav{
+        display:flex;
+        color: $color;
+        border-bottom:1px solid $border-color;
+      &-item{
+        padding:8px 0;
+        margin: 0 16px;
+        cursor:pointer;
+      &:first-child{ margin-left: 0;}
+      &.selected{color:$blue}
+      }
+    }
+    &-content{ padding:8px 0}
+}
+
 
 </style>
