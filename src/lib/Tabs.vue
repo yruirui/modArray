20ba99daa3c80c9030be7e75806b5fd3b32678b9
ruirui <yruirui96@gmail.com>
2024-07-02 14:19:29 +0800

    优化3

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script lang="ts">
-import { computed, ref,onMounted,onUpdated } from 'vue'
+import { computed, ref, watchEffect , onUpdated,} from 'vue'
 import Tab from '../lib/Tab.vue'
 export default{
     props:{
@@ -22,22 +22,20 @@ export default{
     },
     setup(props,context){
        
-       const selectedItem= ref<HTMLDivElement>(null)
+        const selectedItem = ref < HTMLDivElement > (null)
        const indicator=ref<HTMLDivElement>(null)
        const container=ref<HTMLDivElement>(null)
-       const x=()=>{
-        
-        
-        const {width}=selectedItem.value.getBoundingClientRect()
-        indicator.value.style.width=width+'px'
-        const {left:left1}=container.value.getBoundingClientRect()
-        const {left:left2}=selectedItem.value.getBoundingClientRect()
-        const left=left2-left1
-        indicator.value.style.left=left+'px'
-       }
-       onMounted(x);
        
-    onUpdated(x) 
+      watchEffect(()=>{
+        if (selectedItem.value){
+            const {width}=selectedItem.value.getBoundingClientRect()
+            indicator.value.style.width=width+'px'
+            const {left:left1}=container.value.getBoundingClientRect()
+            const {left:left2}=selectedItem.value.getBoundingClientRect()
+            const left=left2-left1
+            indicator.value.style.left=left+'px'
+        } 
+      })
        const defaults=context.slots.default!()
        defaults.forEach((tag)=>{
         if(tag.type!==Tab){
