99021b6c4e96a5c42ff55bdd29b304f07df4d6ce
ruirui <yruirui96@gmail.com>
2024-07-23 19:53:44 +0800

    keyBoard和圣杯布局组件

---
+++ src/components/keyBoartDemo1.vue
@@ -0,0 +1,20 @@
+<demo>键盘组件</demo>
+<template>
+  <keyBoard :Title="Title" v-model:inputContent="y"></keyBoard>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import keyBoard from "../lib/keyBoard.vue";
+export default {
+  components: { keyBoard },
+  setup() {
+    const Title = "请输入密码";
+    const y = ref("");
+    return { Title, y };
+  },
+};
+</script>
+
+<style>
+</style>
